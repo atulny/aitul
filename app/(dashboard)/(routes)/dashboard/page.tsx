847a9f0e31255e3a1c42b0ade18e88f0f65b4fbe
Atul Sharma <atulny@egmail.com>
2023-08-23 23:20:59 -0400

    db styl

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -17,21 +17,21 @@ export default function HomePage() {
         <h2 className="text-2xl mt-6 md:text-4xl font-bold text-center">
           Explore and Experience the power of AI
         </h2>
-       
+
       </div>
       <div className="m-20 grid grid-cols-1 gap-x-10 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
         {tools.map((tool) => (
-          <Card onClick={() => router.push(tool.href)} key={tool.href} className="p-8 pl-2 pb-2 border-black   items-center justify-between hover:shadow-md transition cursor-pointer hover:bg-sky-50" style={{minWidth:"185px"}}>
+          <Card onClick={() => router.push(tool.href)} key={tool.href} className="p-8 pl-2 pb-2 border-black   items-center justify-between hover:shadow-md transition cursor-pointer hover:bg-sky-50" style={{minHeight:"200px",minWidth:"185px"}}>
             <div className="flex items-center gap-x-4">
               <div className={cn("p-2 w-fit rounded-md", tool.bgColor)}>
                 <tool.icon className={cn("w-8 h-8", tool.color)} />
               </div>
               <div className="font-semibold">
                 {tool.label}
-                
+
               </div>
             </div>
-            <div className="font-light text-xs"  style={{marginTop:"8px"}}>
+            <div className="font-light text-xs"  style={{textAlign:"center",paddingTop:"20px"}}>
                   {tool.description}
                 </div>
           </Card>
