42aaca9dad13eb46319ea2cc27e10e0c48c31792
Atul Sharma <atulny@egmail.com>
2023-08-25 17:28:55 -0400

    ui3

--- components/landing-hero.tsx
+++ components/landing-hero.tsx
@@ -14,7 +14,7 @@ export const LandingHero = () => {
     <div className="text-white font-bold py-36 text-center space-y-5">
       <div className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl space-y-5 font-extrabold">
         <h1>AI Toolset</h1>
-        <div className="text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-blue-600 text-base">
+        <div className="text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-blue-600 text-base" style={{minHeight:"30px"}}>
          
           <TypewriterComponent
             options={{
@@ -40,7 +40,7 @@ export const LandingHero = () => {
       <div>
         <Link href={isSignedIn ? "/dashboard" : "/sign-up"}>
           <Button  className="md:text-lg p-4 md:p-6 rounded-full font-semibold">
-            Get Started
+            Get Started: Generate content
           </Button>
         </Link>
       </div>
